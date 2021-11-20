7d39e12b1088f4096f7dc2ac3f7847ed99179205
doongjohn <doongjohn@gmail.com>
2021-11-20 19:38:12 +0900

    update

--- src/nim_magic_square.nim
+++ src/nim_magic_square.nim
@@ -2,8 +2,35 @@
 import staticglfw
 import opengl
 import vmath
+import chroma
 import pixie
 import display
+
+
+const white = parseHex "ffffff"
+const color1 = parseHex "bfdbff"
+const color2 = parseHex "ededed"
+
+var font = readFont("C:/Windows/Fonts/consola.ttf")
+font.size = 30
+
+
+type Tile = object
+  color: Color
+  data: string
+
+const grid = [
+  [Tile(color: color1), Tile(color: color2), Tile(color: color1)],
+  [Tile(color: color2), Tile(color: color1), Tile(color: color2)],
+  [Tile(color: color1), Tile(color: color2), Tile(color: color1)],
+]
+
+
+proc getCursorPos: Vec2 =
+  var x, y: cdouble
+  window.getCursorPos(x.addr, y.addr)
+  result.x = x
+  result.y = y
 
 
 discard setMouseButtonCallback(window, proc(window: Window, button: cint, action: cint, modifiers: cint) {.cdecl.} =
@@ -16,18 +43,13 @@ discard setMouseButtonCallback(window, proc(window: Window, button: cint, action
   if button == MOUSE_BUTTON_LEFT:
     stdout.write "LMB "
     case action
-    of PRESS: echo "down"
+    of PRESS:
+      echo "down"
+      let mousePos = getCursorPos()
+      echo mousePos
     of RELEASE: echo "up"
     else: discard
 )
-
-
-const white = parseHex "ffffff"
-const color1 = parseHex "ededed"
-const color2 = parseHex "bfdbff"
-
-var font = readFont("C:/Windows/Fonts/consola.ttf")
-font.size = 30
 
 
 proc drawTile(x, y: float) =
@@ -38,17 +60,14 @@ main:
   ctx.fillStyle = white
   drawRect center.x, center.y, screenSize.w.float, screenSize.h.float
 
-  ctx.fillStyle = color1
-  drawTile center.x, center.y + 100
-  drawTile center.x, center.y - 100
-  drawTile center.x + 100, center.y
-  drawTile center.x - 100, center.y
+  var pos = vec2(center.x - 100, center.y - 100)
 
-  ctx.fillStyle = color2
-  drawTile center.x, center.y
-  drawTile center.x - 100, center.y + 100
-  drawTile center.x + 100, center.y + 100
-  drawTile center.x - 100, center.y - 100
-  drawTile center.x + 100, center.y - 100
-
-  screen.fillText(font, "1", translate(center), vec2(0, 0), haCenter, vaMiddle)
+  for row in grid:
+    for tile in row:
+      ctx.fillStyle = tile.color
+      drawTile pos.x, pos.y
+      if tile.data != "":
+        screen.fillText(font, tile.data, translate(pos), vec2(0, 0), haCenter, vaMiddle)
+      pos.x += 100
+    pos.x = center.x - 100
+    pos.y += 100
